86f11656f86bb5744606a0c1e436c2af1deb93c3
Christopher de Oliveira <christopherforwork@gmail.com>
2021-03-21 14:02:52 -0400

    🔖 (0.1): First version of the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import styled from 'styled-components';
 
-function App() {
+const Site = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #333;
+`;
+
+const CalcArea = styled.div`
+  display: flex;
+  flex-flow: column;
+  padding: 20px;
+  width: 400px;
+  height: 400px;
+  border-radius: 10px;
+  background-color: #fff;
+  text-align: center;
+`;
+
+const CalcTitle = styled.h1`
+  font-size: 25px;
+  font-weight: normal;
+`;
+
+const CalcInputArea = styled.div`
+  flex: 3;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-flow: column;
+`;
+
+const CalcLabel = styled.label`
+  width: 100%;
+  align-self: flex-start;
+  padding: 10px;
+  text-align: start;
+  :last-child{
+    margin-top: 10px;
+  };
+`;
+
+const CalcInput = styled.input`
+  display: block;
+  padding: 10px;
+  width: 90%;
+  margin-top: 5px;
+`;
+
+const HorizontalRow = styled.div`
+  height: 1px;
+  width: 100%;
+  background-color: #333;
+`;
+
+const CalcResultArea = styled.div`
+  flex: 2;
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CalcResult = styled.div`
+  padding: 2px;
+  :last-child{
+    font-size: 20px;
+    font-weight: bold;
+  }
+`;
+
+const App = () => {
+  const [amount, setAmount] = useState(0);
+  const [tipPercent, setTipPercent] = useState(10);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Site>
+      <CalcArea>
+        <CalcTitle>Calculadora de Gorjeta</CalcTitle>
+        <CalcInputArea>
+          <CalcLabel>
+            Valor da conta:
+            <CalcInput autoFocus type='number' value={amount} onChange={e => setAmount(e.target.value)} />
+          </CalcLabel>
+          <CalcLabel>
+            Porcentagem de gorjeta:
+            <CalcInput type='number' value={tipPercent} onChange={e => setTipPercent(e.target.value)} />
+          </CalcLabel>
+        </CalcInputArea>
+
+        <HorizontalRow />
+
+        <CalcResultArea>
+          {(amount > 0 && tipPercent > 0) ?
+            <>
+              <CalcResult>Sub-total: R${parseFloat(amount).toFixed(2)}</CalcResult>
+              <CalcResult>Gorjeta ({tipPercent}%): R${parseFloat(amount * tipPercent / 100).toFixed(2)}</CalcResult>
+              <CalcResult>Total: R${(parseFloat(amount) + parseFloat(amount * tipPercent / 100)).toFixed(2)}</CalcResult>
+            </>
+            :
+            <CalcResult>Total: R$0.00</CalcResult>
+          }
+        </CalcResultArea>
+      </CalcArea>
+    </Site>
   );
-}
+};
 
 export default App;
